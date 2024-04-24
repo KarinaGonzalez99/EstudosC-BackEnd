f61b3db421e0d8e134f0329a65020a94d4688095
KarinaGonzalezWM <p22.karina@webmotors.com.br>
2024-04-24 09:54:48 -0300

    Ajusta files em pastas

---
+++ C# parte 1 - Primeiros Passos/3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3_CriandoVariaveisPontoFlutuante {
+	class Program {
+		static void Main(string[] args) {
+			//criando variaveis ponto fluante
+			Console.WriteLine("Executando projeto 3 - Criando Variaveis Ponto Flutuante ");
+			double salario;
+			salario = 1000.70;
+
+			Console.WriteLine(salario);
+
+			int brindes = 15;
+			int pessoas = 2;
+			Console.WriteLine(brindes / pessoas); // 7
+		}
+	}
+}
