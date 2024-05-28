7e3f1c8031c52ecbc8580b81476590d67db431f1
KarinaGonzalezWM <p22.karina@webmotors.com.br>
2024-05-28 14:29:26 -0300

    Finaliza API Rest com .NET 5: operações essenciais com verbos HTTP

---
+++ API Rest com .NET 5/FilmesAPI/Data/FilmeContext.cs
@@ -0,0 +1,17 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Data
+{
+	public class FilmeContext : DbContext
+	{
+		// quais as opçõoes desse contexto, que é o do filme 
+		public FilmeContext(DbContextOptions<FilmeContext> opt) : base(opt)
+		{
+
+		}
+
+		// referencia ao banco de dados Filme
+		public DbSet<Filme> Filmes { get; set; }
+	}
+}
